bebf3786180e55b2c988f165c224b6c058be8bcb
Mukhamad Ali Sya'bani <asyabani650@gmail.com>
2026-07-17 21:26:06 +0700

    redeploy3

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import express, { type Express, type Request, type Response } from "express";
+import express from "express";
+import authRouter from "./routes/auth/index.js";
+import usersRouter from "./routes/user/index.js";
+import suratMasukRouter from "./routes/suratMasuk/index.js";
+import suratKeluarRouter from "./routes/suratKeluar/index.js";
+import guestRouter from "./routes/guest/index.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app: Express = express();
-const port = 3000;
+dotenv.config();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// middleware umum
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "https://security-app-roan.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+// roter auth
+app.use("/api", authRouter);
+// router user,
+app.use("/api/users", usersRouter);
+// router guest
+app.use("/api/guests", guestRouter);
+// router surat masuk
+app.use("/api/surat-masuk", suratMasukRouter);
+// router surat keluar
+app.use("/api/surat-keluar", suratKeluarRouter);
+
+export default app;
